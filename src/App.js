068a86d214a0cbe5296e476e410163eea4f3a311
ivan.puhachov <puhachov@ut.ee>
2025-08-04 17:05:53 +0300

    Added scrolling to the top of the page when the user opens new page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Main App component for MindinBlue website
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "./contexts/LanguageContext";
+import ScrollToTop from "./components/ScrollToTop";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
@@ -15,13 +16,17 @@ function App() {
   return (
     <LanguageProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/team" element={<Team />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/counselling-psychotherapy" element={<CounsellingPsychotherapy />} />
+          <Route
+            path="/counselling-psychotherapy"
+            element={<CounsellingPsychotherapy />}
+          />
           <Route path="/couples-therapy" element={<CouplesTherapy />} />
           <Route path="/online-consultation" element={<OnlineConsultation />} />
         </Routes>
